8de5f728b2ce2aba9e3284d35b6df3ea0dc64a95
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-12-15 13:50:05 +0200

    fix: remove uninitialized variables

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,4 @@
 class User < ApplicationRecord
-  pay_customer
-
-  has_person_name
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
