65da732c65b528c1f43a65513f2659e43d288511
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-12-15 08:26:55 +0200

    update: routes configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,13 @@
+require "sidekiq/web"
+
 Rails.application.routes.draw do
-  get "posts/index"
-  get "posts/create"
-  get "posts/destroy"
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Sidekiq::Web => "/sidekiq"
+  end
+
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :posts, except: [ :edit, :update ]
+  root "posts#index"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
