d2a2eb4877d151c12a9ab8443af3628d2d740358
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-12-15 16:19:47 +0200

    update routes configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 require "sidekiq/web"
 
 Rails.application.routes.draw do
-  resources :profiles
   authenticate :user, lambda { |u| u.admin? } do
     mount Sidekiq::Web => "/sidekiq"
   end
 
   devise_for :users
   resources :posts, except: [ :edit, :update ]
+  resources :profiles
   root "posts#index"
 
   get "up" => "rails/health#show", as: :rails_health_check
