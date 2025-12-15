42e8e455c138bf23cb08c1acb7df5b57f201629a
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-12-15 10:10:19 +0200

    feat: add logic to create a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,29 @@
 class PostsController < ApplicationController
   def index
+    @post = Post.new
+    @posts = Post.all.order(created_at: :asc)
   end
 
   def create
+    @post = Post.new(post_params)
+    respond_to do |format|
+      if @post.save
+        format.turbo_stream
+      else
+        format.html do
+          flash[:post_errors] = @post.errors.full_messages
+          redirect_to root_path
+        end
+      end
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:tweet).permit(:body)
   end
 end
