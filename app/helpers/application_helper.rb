a4a79c2695627f54e20edc831dc7000964591879
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-12-15 14:15:22 +0200

    Add profile image helper and profile show view
    - Add profile_image helper with size options (large, small, default)
    - Create profiles/show.html.erb with user profile display
    - Display profile avatar, name, username, and posts
    - Add Edit profile button for current user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -29,4 +29,23 @@ module ApplicationHelper
   def link_button_class
     "font-medium text-sky-500 hover:text-sky-600 transition ease-in-out duration-300"
   end
+
+  def profile_image(user, options = {})
+    size = case options[:size]
+           when "large"
+             "w-20 h-20"
+           when "small"
+             "w-10 h-10"
+           else
+             "w-14 h-14"
+           end
+
+    classes = "#{size} flex-shrink-0 rounded-full border-2 border-white"
+
+    if user.profile_image.attached?
+      image_tag user.profile_image, class: classes
+    else
+      image_tag "https://doodleipsum.com/700/avatar-5?bg=3D27F6&i=f339578a64040310d3eb5bd82b550627", class: classes
+    end
+  end
 end
