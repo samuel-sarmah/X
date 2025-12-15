ed80429b7724e2d81f685fa1ae94ef1a4310a0f8
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-12-15 08:23:14 +0200

    feat: generate posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "posts/index"
+  get "posts/create"
+  get "posts/destroy"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
