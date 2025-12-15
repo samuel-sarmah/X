c5a8d9c9baf22cdd029f854a062cb59ff6dcc7f8
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-12-15 13:27:49 +0200

    feat: add profile image logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  pay_customer
+
+  has_person_name
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,4 +10,6 @@ class User < ApplicationRecord
 
   has_many :posts
   validates_uniqueness_of :username
+
+  has_one_attached :profile_image
 end
