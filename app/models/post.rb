7e0843be0a3a5f597dcbc275951c90ed6418b9e8
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-12-17 11:21:50 +0200

    update: add repost validation and logic

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,16 @@
 class Post < ApplicationRecord
   belongs_to :user
+  belongs_to :post, optional: true # means a post can be created without another post association.
 
   validates :body, length: { maximum: 240 }, allow_blank: false
+
+  def post_type
+    if post_id? && body?
+      "quote-post"
+    elsif post_id?
+      "repost"
+    else
+      "post"
+    end
+  end
 end
